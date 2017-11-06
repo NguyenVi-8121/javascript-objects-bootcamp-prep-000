ec94e0608fbb453c33035fab6f0434616cea4c50
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-11-06 18:39:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,11 @@ var playlist = {
   'Ke$ha': 'Praying',
   'Kanye': 'Gold Digger'
 };
+function updatePlaylist(playlist, 'Phil Ochs', 'Heres to the State of Mississippi'){
+  playlist.['Phil Ochs'] = 'Heres to the State of Mississippi';
+  return updatePlaylist
+}
+function removeFromPlaylist(playlist, Kanye, Gold Digger){
+  delete playlist.Kanye
+  return playlist
+}
