99522f790fde900fd685e65cfbe59427a84bd62c
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-11-06 18:08:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = Playlist ({Ke$ha:'praying', Kanye:"Gold Digger"});
-function updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi"){
+function updatePlaylist(Playlist, 'Phil Ochs', "Here's to the State of Mississippi"){
   return playlist.assign({Phil Ochs: 'Here\'s to the State of Mississippi'});
 }
