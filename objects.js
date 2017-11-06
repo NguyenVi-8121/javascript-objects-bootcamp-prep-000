76182f77b57427e184933251f75a57386925c812
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-11-06 19:45:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
   return playlist
 }
-function removeFromPlaylist(playlist){
-  delete playlist.Kanye
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName
   return playlist
 }
